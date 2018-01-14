f8e3478d9c2d79f7b5631193c14d335accc7d4bf
binio <tbiniecki@gmail.com>
2018-01-14 16:29:38 +0000

    add book properties

--- src/main/java/binio/springframework/spring5webapp/model/Book.java
+++ src/main/java/binio/springframework/spring5webapp/model/Book.java
@@ -1,7 +1,26 @@
 package binio.springframework.spring5webapp.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String publisher;
+    private String isbn;
+
+    public Book() {
+    }
+
+    public Book(String title, String publisher, String isbn) {
+        this.title = title;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
 }
