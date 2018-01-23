950752f6a72bd860d411c0eab4ff05f091cef3ea
binio <tbiniecki@gmail.com>
2018-01-23 23:11:52 +0000

    add controller

---
+++ src/main/java/binio/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,22 @@
+package binio.springframework.spring5webapp.controllers;
+
+import binio.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
