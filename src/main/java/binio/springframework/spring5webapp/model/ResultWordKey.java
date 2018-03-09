876a906431eedb70a590d836e84d109288f2b3d6
binio <tbiniecki@gmail.com>
2018-03-08 20:00:00 -0600

    add key

--- src/main/java/binio/springframework/spring5webapp/model/ResultWordKey.java
+++ src/main/java/binio/springframework/spring5webapp/model/ResultWordKey.java
@@ -1,4 +1,14 @@
 package binio.springframework.spring5webapp.model;
 
-public class ResultWordKey {
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class ResultWordKey implements Serializable{
+
+    @Id
+    private Long idNumber;
+    @Id
+    private Long month;
+    @Id
+    private String hashCode;
 }
