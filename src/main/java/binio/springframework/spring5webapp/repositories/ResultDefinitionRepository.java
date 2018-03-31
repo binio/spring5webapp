17b10975e6ecdb29d14b48f9c405d2b5a7fc7282
binio <tbiniecki@gmail.com>
2018-03-31 12:27:24 +0100

    add repo changes

--- src/main/java/binio/springframework/spring5webapp/repositories/ResultDefinitionRepository.java
+++ src/main/java/binio/springframework/spring5webapp/repositories/ResultDefinitionRepository.java
@@ -1,7 +1,8 @@
 package binio.springframework.spring5webapp.repositories;
 
 import binio.springframework.spring5webapp.model.ResultDefinition;
+import binio.springframework.spring5webapp.model.ResultDefinitionKey;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ResultDefinitionRepository  extends CrudRepository<ResultDefinition, Long> {
+public interface ResultDefinitionRepository  extends CrudRepository<ResultDefinition, ResultDefinitionKey> {
 }
