3bfdd9a5d2c3532f296d932f2050e095cea26d5b
binio <tbiniecki@gmail.com>
2018-03-31 12:25:36 +0100

    add entities

--- src/main/java/binio/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/binio/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -1,14 +1,17 @@
 package binio.springframework.spring5webapp.bootstrap;
 
-import binio.springframework.spring5webapp.model.Author;
-import binio.springframework.spring5webapp.model.Book;
-import binio.springframework.spring5webapp.model.Publisher;
+import binio.springframework.spring5webapp.model.*;
 import binio.springframework.spring5webapp.repositories.AuthorRepository;
 import binio.springframework.spring5webapp.repositories.BookRepository;
 import binio.springframework.spring5webapp.repositories.PublisherRepository;
+import binio.springframework.spring5webapp.repositories.ResultDefinitionRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
@@ -16,15 +19,60 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
     private final PublisherRepository publisherRepository;
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private ResultDefinitionRepository resultDefinitionRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository,ResultDefinitionRepository resultDefinitionRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
         this.publisherRepository = publisherRepository;
+        this.resultDefinitionRepository = resultDefinitionRepository;
     }
 
     private void initData(){
 
+        UUID resultDefinitionId = UUID.randomUUID();
+        UUID resultPromptId = UUID.randomUUID();
+        ZonedDateTime resultDefinitionVersion = ZonedDateTime.now();
+
+        ArrayList<ResultPromptWordGroup> resultPromptWordGroups = new ArrayList<>();
+        ArrayList<ResultPrompt> prompts = new ArrayList<>();
+
+        ResultPromptWordGroup resultPromptWordGroup = new ResultPromptWordGroup(
+                "resultWord",
+                "123",
+                resultPromptId,
+                resultDefinitionVersion,
+                resultDefinitionId
+
+        );
+        ResultPromptWordGroup resultPromptWordGroup1 = new ResultPromptWordGroup(
+                "resultWord-1",
+                "123",
+                resultPromptId,
+                resultDefinitionVersion,
+                resultDefinitionId
+
+        );
+        resultPromptWordGroups.add(resultPromptWordGroup);
+        resultPromptWordGroups.add(resultPromptWordGroup1);
+
+        ResultPrompt prompt = new ResultPrompt(
+                resultPromptId,
+                resultDefinitionId,
+                resultDefinitionVersion,
+                "promptLabel",
+                resultPromptWordGroups
+        );
+
+        prompts.add(prompt);
+
+        ResultDefinition rd = new ResultDefinition(
+                resultDefinitionId,
+                "name",
+                "description",
+                prompts,
+                resultDefinitionVersion
+        );
         Publisher publisher = new Publisher();
         publisher.setName("foo");
         publisherRepository.save(publisher);
@@ -55,6 +103,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
         //Adam
         Author adam = new Author("Adam", "Binieckiz");
         authorRepository.save(adam);
+
+        resultDefinitionRepository.save(rd);
     }
 
     @Override
